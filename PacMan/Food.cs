a511adfa256735d71d7763d8a979728c4fc37670
Mathias Rogey <mathias.rogey@eduvaud.ch>
2020-08-05 22:02:23 +0200

    Creating the way I will creat the visual of the map, and I am still thinking how I am going to do the food

---
+++ PacMan/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    public partial class Food
+    {
+        public char Type { get; }
+
+        public Food(char type)
+        {
+            Type = type;
+        }
+
+        public struct FoodMeaning
+        {
+            public const char NORMALFOOD = 'f';
+            public const char BIGFOOD = 'b';
+        }
+    }
+}
--- PacMan/Map.cs
+++ PacMan/Map.cs
@@ -1,27 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PacMan.Variables;
 
 namespace PacMan
 {
     public class Map
     {
+        public const int MapWidth = 21;
+        public const int MapHeight = 19;
+
+        public void DrawMapRectangle(Panel panel, char type, int x, int y)
+        {
+            Color color;
+
+            Graphics graphics = panel.CreateGraphics();
+
+            switch (type)
+            {
+                case MapMeaning.EMPTY:
+                    color = MapColor.EMPTY;
+                    break;
+
+                case MapMeaning.WALL:
+                    color = MapColor.WALL;
+                    break;
+
+                case MapMeaning.ROAD:
+                    color = MapColor.ROAD;
+                    break;
+
+                case MapMeaning.NORMALFOOD:
+                case MapMeaning.BIGFOOD:
+                    color = MapColor.BIGFOOD;
+                    break;
+
+                case MapMeaning.TELEPORTATION:
+                    color = MapColor.TELEPORTATION;
+                    break;
+
+                default:
+                    return;
+            }
+
+            Rectangle rectangle = new Rectangle(x, y, G_BYTESIZEOFSQUARE, G_BYTESIZEOFSQUARE);
+
+            graphics.DrawRectangle(new Pen(color, 1), rectangle);
+
+            graphics.FillRectangle(new SolidBrush(color), rectangle);
+        }
+
         public struct MapMeaning
         {
             public const char EMPTY = 'e';
             public const char WALL = 'w';
             public const char ROAD = 'r';
-            public const char FOOD = 'f';
+            public const char NORMALFOOD = 'f';
             public const char BIGFOOD = 'b';
             public const char TELEPORTATION = 't';
-        } 
+        }
+
+        private struct MapColor
+        {
+            public readonly static Color EMPTY = Color.Black;
+            public readonly static Color WALL = Color.DarkBlue;
+            public readonly static Color ROAD = Color.Black;
+            public readonly static Color FOOD = Color.White;
+            public readonly static Color BIGFOOD = FOOD;
+            public readonly static Color TELEPORTATION = Color.Yellow;
+        }
 
         // 17 no border, 19 with border, 21 with tp
 
         /*
-        public char[,] GameMap { get; set; } = new char[19, 21]
+        public char[,] GameMap { get; set; } = new char[MapHeight, MapWidth]
         {
             {'e', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'e' }, //1
             {'e', 'w', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'w', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'w', 'e' }, //2
@@ -29,7 +85,7 @@ namespace PacMan
             {'e', 'w', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'w', 'e' }, //4
             {'e', 'w', 'f', 'w', 'w', 'f', 'w', 'f', 'w', 'w', 'w', 'w', 'w', 'f', 'w', 'f', 'w', 'w', 'f', 'w', 'e' }, //5
             {'e', 'w', 'f', 'f', 'f', 'f', 'w', 'f', 'f', 'f', 'w', 'f', 'f', 'f', 'w', 'f', 'f', 'f', 'f', 'w', 'e' }, //6
-            {'e', 'w', 'w', 'w', 'w', 'f', 'w', 'w', 'w', 'r', 'w', 'r', 'w', 'w', 'w', 'f', 'w', 'w', 'w', 'w', 'e' } ,//7
+            {'e', 'w', 'w', 'w', 'w', 'f', 'w', 'w', 'w', 'r', 'w', 'r', 'w', 'w', 'w', 'f', 'w', 'w', 'w', 'w', 'e' }, //7
             {'e', 'e', 'e', 'e', 'w', 'f', 'w', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'w', 'f', 'w', 'e', 'e', 'e', 'e' }, //8
             {'w', 'w', 'w', 'w', 'w', 'f', 'w', 'r', 'w', 'w', 'w', 'w', 'w', 'r', 'w', 'f', 'w', 'w', 'w', 'w', 'w' }, //9
             
@@ -47,7 +103,7 @@ namespace PacMan
         };
 
         */
-        public static char[,] GameMap { get; set; } = new char[21, 19]
+        public static char[,] GameMap { get; set; } = new char[MapWidth, MapHeight]
         {
             {'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'w', 't', 'w', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', },
             {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'e', 'w', 'r', 'w', 'e', 'w', 'w', 'w', 'w', 'w', 'w', 'w', },
